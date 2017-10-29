7ed0435cc8f83564005a5918e13e4875882b17f4
Vitebo <vitebo51@gmail.com>
2017-10-29 12:18:09 -0200

    Criando funcionalidade para receber clientes conectados

--- src/bilac/com/main/Server.java
+++ src/bilac/com/main/Server.java
@@ -1,9 +1,29 @@
 package bilac.com.main;
 
-public class Server {
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server extends Thread {
+  
+  @Override 
+  public void run() {
+    super.run();
+    try {
+      @SuppressWarnings("resource")
+      ServerSocket aguardaCliente = new ServerSocket(4811);
+      while(true) {
+        Socket cliente = aguardaCliente.accept();
+        Scanner scanner = new Scanner(cliente.getInputStream());
+        PrintWriter writer = new PrintWriter(cliente.getOutputStream());
+        System.out.println("Novo cliente conectado");
+      }
+    } catch (Exception e) { System.err.println(e.getMessage()); }
+  }
   
   public static void main(String[] args) {
-    
+    new Server().start();
   }
   
 }
